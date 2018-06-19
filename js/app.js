e30ed9a70629d4b459299e00314b7e8358461d28
Ben Van Meter <vanmeterx@gmail.com>
2018-06-19 13:09:07 -0500

    starting to add javascript coding, after brainstorming with ethan

--- js/app.js
+++ js/app.js
@@ -4,3 +4,16 @@ var imageOne = document.getElementById('imageOne');
 var imageTwo = document.getElementById('imageTwo');
 var imageThree = document.getElementById('imageThree');
 
+
+//in-class assistance
+name imageChoice (name, src, timesViewed, timesClicked);
+  this.name = name;
+  this.src = src;
+  this.timesViewed = timesViewed;
+  this.timesShown = timesShown;
+
+
+ //where images will be organized:
+function initialize() {
+
+}
