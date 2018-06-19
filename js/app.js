1a93fa72825592538eef70f44e4f8f01b64d0d1b
Ben Van Meter <vanmeterx@gmail.com>
2018-06-19 13:14:41 -0500

    added my typed-out charts js notes from lecture

--- js/app.js
+++ js/app.js
@@ -17,3 +17,59 @@ name imageChoice (name, src, timesViewed, timesClicked);
 function initialize() {
 
 }
+
+
+
+//typed out from class
+// let's start a chart 
+
+function showResultChart() {
+  var canvas = document.getElementById('resultsCanvas');
+}
+
+  //unhide our canvas
+  canvas.style.display = 'block';
+
+  //need to build up a list of labels
+
+  var labels = [];
+  var voteCounts = [];
+  var showCounts = [];
+  for (var i = 0; i < Placeholder.all.length: i++) {
+    labels [i] = Placeholder.all[i].name;
+  }
+
+  var ctx = canvas.getContext('2d'); 
+  //ctx is variable for canvas
+
+  var chart = new chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Vote Count'],
+      datasets: [{
+        label: labels,
+        datasets: [{
+          label: 'Vote Count',
+          background
+          data: voteCounts
+        }]
+      }]
+    };
+    options: {
+      responsive: true;
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+      title: {
+        dispaly: true;
+        text: 'Voting Results'
+    }
+  });
+}
+
+//now to add the data
+
