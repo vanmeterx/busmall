9711463eae1e36bd9e0e0d544870de4707422ba5
Ben Van Meter <vanmeterx@gmail.com>
2018-06-18 22:35:50 -0500

    added some base level javascript. but mental road block is huge

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-"use strict";
+'use strict';
+
+var imageOne = document.getElementById('imageOne');
+var imageTwo = document.getElementById('imageTwo');
+var imageThree = document.getElementById('imageThree');
+
